153f0331a9ade5013afa352186997a042b9400a9
ImAdityaa12 <adityagupta1291@gmail.com>
2025-07-31 21:20:01 +0530

    feat(dashboard): add sidebar toggle button to navbar
    Implement toggle functionality for sidebar in dashboard navbar using Button component and sidebar hook. Shows different icons based on sidebar state (collapsed/expanded) and mobile view.

--- src/modules/dashboard/ui/components/dashboard-navbar.tsx
+++ src/modules/dashboard/ui/components/dashboard-navbar.tsx
@@ -1,9 +1,27 @@
 'use client';
 
+import { Button } from '@/components/ui/button';
+import { useSidebar } from '@/components/ui/sidebar';
+import { PanelLeftCloseIcon, PanelLeftIcon } from 'lucide-react';
 import React from 'react';
 
 const DashboardNavbar = () => {
-    return <nav>DashboardNavbar</nav>;
+    const { state, toggleSidebar, isMobile } = useSidebar();
+    return (
+        <nav className="flex px-4 gap-x-2 items-center py-3 border-b bg-background">
+            <Button
+                className="size-9"
+                variant={'outline'}
+                onClick={toggleSidebar}
+            >
+                {state === 'collapsed' || isMobile ? (
+                    <PanelLeftIcon className="size-4" />
+                ) : (
+                    <PanelLeftCloseIcon className="size-4" />
+                )}
+            </Button>
+        </nav>
+    );
 };
 
 export default DashboardNavbar;
