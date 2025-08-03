ff42e09a95baa5f93bb3734a97b1169af83fba6e
ImAdityaa12 <adityagupta1291@gmail.com>
2025-08-03 10:28:07 +0530

    feat(agents): add error boundary and suspense for agents loading
    Implement suspense and error boundary patterns for agents loading to improve user experience. Move loading and error states to page level and use suspense for data fetching in the view component.

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -1,8 +1,27 @@
+import LoadingState from '@/components/loading-state';
 import { AgentsView } from '@/modules/agents/ui/views/agents-view';
-import React from 'react';
+import { getQueryClient, trpc } from '@/trpc/server';
+import { HydrationBoundary, dehydrate } from '@tanstack/react-query';
+import React, { Suspense } from 'react';
 
 const page = () => {
-    return <AgentsView />;
+    const queryClient = getQueryClient();
+    void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
+
+    return (
+        <HydrationBoundary state={dehydrate(queryClient)}>
+            <Suspense
+                fallback={
+                    <LoadingState
+                        title="Loading agents"
+                        description="We are loading your agents. Please wait."
+                    />
+                }
+            >
+                <AgentsView />
+            </Suspense>
+        </HydrationBoundary>
+    );
 };
 
 export default page;
--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -1,33 +1,11 @@
 'use client';
 
-import ErrorState from '@/components/error-state';
-import LoadingState from '@/components/loading-state';
 import { useTRPC } from '@/trpc/client';
-import { useQuery } from '@tanstack/react-query';
+import { useSuspenseQuery } from '@tanstack/react-query';
 
 export const AgentsView = () => {
     const trpc = useTRPC();
-    const {
-        data: agents,
-        isLoading,
-        isError,
-    } = useQuery(trpc.agents.getMany.queryOptions());
+    const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
 
-    if (isLoading) {
-        return (
-            <LoadingState
-                title="Loading agents"
-                description="We are loading your agents. Please wait."
-            />
-        );
-    }
-    if (isError) {
-        return (
-            <ErrorState
-                title="Error loading agents"
-                description="We were unable to load your agents. Please try again later."
-            />
-        );
-    }
-    return <div>{JSON.stringify(agents, null, 2)}</div>;
+    return <div>{JSON.stringify(data, null, 2)}</div>;
 };
