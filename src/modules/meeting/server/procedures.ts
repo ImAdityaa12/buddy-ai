363a0d28e91b25da8540d54fe7466ef3d846e3a8
ImAdityaa12 <adityagupta1291@gmail.com>
2025-08-10 23:49:00 +0530

    feat(meetings): add agent details and duration to meeting query
    Include agent information and calculate meeting duration in seconds when fetching meeting details to provide more comprehensive meeting data.

--- src/modules/meeting/server/procedures.ts
+++ src/modules/meeting/server/procedures.ts
@@ -65,8 +65,14 @@ export const meetingsRouter = createTRPCRouter({
             const [data] = await db
                 .select({
                     ...getTableColumns(meetings),
+                    agent: agents,
+                    duration:
+                        sql<number>`EXTRACT(EPOCH FROM (ended_at - started_at))`.as(
+                            'duration'
+                        ),
                 })
                 .from(meetings)
+                .innerJoin(agents, eq(meetings.agentId, agents.id))
                 .where(
                     and(
                         eq(meetings.id, input.id),
